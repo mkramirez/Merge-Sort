08f294e22f2427372b825b45d64ebf4d26ac48e5
da-be-at <mkramirez1992@gmail.com>
2017-10-16 20:58:32 -0700

    Final Commit

--- main.cpp
+++ main.cpp
@@ -1,96 +1,70 @@
-#include <iostream>
 #include <cassert>
+#include <iostream>
+#include <vector>
 
-using namespace std;
+void merge(int a[], int start, int mid, int end) {
+    int n = end - start + 1;
+    std::vector<int> b(n);
+    int i1 = start;
+    int i2 = mid + 1;
+    int j = 0;
 
-// s = start of range to sort
-// e = end of range to sort
-void merge(int a[], int x[], int s, int e, int m) {
-
-    int i, j, k;
-    i = s;
-    k = 0;
-    j = m + 1;
-
-    while (i <= m && j <= e)
-    {
-        if (a[i] < a[j])
-        {
-            x[k] = a[i];
-            k++;
-            i++;
+    while (i1 <= mid && i2 <= end) {
+        if (a[i1] < a[i2]) {
+            b[j] = a[i1];
+            i1++;
         }
-        else
-        {
-            x[k] = a[j];
-            k++;
-            j++;
+        else {
+            b[j] = a[i2];
+            i2++;
         }
-    }
-
-    while (i <= m)
-    {
-        x[k] = a[i];
-        k++;
-        i++;
-    }
-
-
-    while (j <= e)
-    {
-        x[k] = a[j];
-        k++;
         j++;
     }
 
-    //allocate data back from x[] into a[].
-    for (i = s; i <= e; i++)
-    {
-        a[i] = x[i-s];
+    while (i1 <= mid) {
+        b[j] = a[i1];
+        i1++;
+        j++;
     }
-}
-
-void msort (int a[], int x[], int s, int e) {
-    if (e - s < 1){
-        return;
+    while (i2 <= end) {
+        b[j] = a[i2];
+        i2++;
+        j++;
     }
-    int m = (e + s)/2;
-    msort(a, x, s, m);
-    msort(a, x, m + 1, e);
-    merge(a, x, s, e, m);
+    for (j = 0; j < n; j++)
+        a[start + j] = b[j];
 }
 
-void mergesort(int a[], int n) {
-    int *x = new int[n];
-    msort(a,x,0, n-1);
-    delete []x;
+void merge_sort(int a[], int from, int to) {
+    if (from == to) return;
+    int mid = (from + to) / 2;
+    merge_sort(a, from, mid);
+    merge_sort(a, mid + 1, to);
+    merge(a, from, mid, to);
 }
 
-bool sorted(int a[], int n) {
-    for (int i=0; i<n-1; ++i){
-        if (a[i] > a[i+1])
+bool sorted (int a[], int n) {
+    for (int i = 0; i < n -1; ++i) {
+        if (a[i] > a[i + 1])
             return false;
     }
     return true;
 }
 
-int main(int argc, char * args[]) {
+int main (int argc, char * args[]) {
     int a[1000];
     for (int i = 0; i < 1000; ++i) a[i] = -50 + rand() % 100;
-    mergesort(a, 1000);
+    merge_sort(a, 0, 999);
     assert(sorted(a, 1000));
-
     int b[1001];
     for (int i = 0; i < 1001; ++i) b[i] = -50 + rand() % 100;
-    mergesort(b, 1001);
+    merge_sort(b, 0, 1000);
     assert(sorted(b, 1001));
-
     int c[] = { 2 };
-    mergesort(c, 1);
+    merge_sort(c, 0, 1);
     assert(sorted(c, 1));
-
     int d[] = { 1, 2, 3, 4, 5 };
-    mergesort(d, 5);
+    merge_sort(d, 0, 4);
     assert(sorted(d, 5));
-    cout << "All tests passed." << endl;
+    std::cout << "All tests passed." << std::endl;
 }
